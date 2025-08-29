cf506c8a7451ea0a186cd3815ca0a64a0c2c1088
hamnaakram721-source <hamnaakram721@gmail.com>
2025-08-29 01:46:23 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './assets/component/nav/nav';
+import Section from './assets/component/section/Section';
+function App() {
+  return (
+    <div>
+<Nav/>
+<Section/>
+    </div>
+  )
+}
+
+export default App
